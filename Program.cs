f29b328b096c6b8920a28e85faef7769b3c5bde5
Eduardo Pereira de Amorim <145592635+AldebaraFork@users.noreply.github.com>
2023-12-09 18:35:37 -0300

    Conditional Structure
    started with a simple code

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.ComponentModel.Design;
+using System.Diagnostics.CodeAnalysis;
+using System.IO.Pipes;
+using System.Security.Cryptography;
+using System.Globalization;
+using System.Xml.Schema;
+
+
+Console.WriteLine("Ola bem vindo! Por favor Insira seu nome: ");
+string[] dados = Console.ReadLine().Split(' ');
+
+string nome = dados[0];
+Console.WriteLine("Otimo! Seja bem vindo(a) " + nome + " Porfavor insira sua idade: ");
+int idade = int.Parse(Console.ReadLine());
+if (idade >= 18)
+{
+    Console.WriteLine("Seja bem Vindo! Pode utilizar nossa plataforma a vontade! ");
+}
+else
+{
+    Console.WriteLine("Opa! parece que voce é de menor, voce possui apenas " + idade);
+}
+
